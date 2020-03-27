521bdd1fb220684d7d7bb4bc1a8d8b21e72ccac0
Retsuki <retu1118@gmail.com>
2020-03-27 22:24:15 +0900

    データ更新2020/03/27 22:24

--- run-regularly.sh
+++ run-regularly.sh
@@ -1,6 +1,6 @@
 today=`date "+%Y/%m/%d %H:%M"`
 git pull origin development
-python3 tool/scraping.py
+python3 /home/sakura/covid19-ishikawa/tool/scraping-cent.py
 git add .
 git commit -m "データ更新${today}"
 git push origin development
