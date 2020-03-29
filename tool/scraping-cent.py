d24f2cfef96442a85b24b293558df9bf4ac8b5ef
Retsuki <s1f101701845@iniad.org>
2020-03-29 12:05:32 +0900

    Delete file

--- tool/scraping-cent.py
+++ tool/scraping-cent.py
@@ -1,5 +1,5 @@
+import os
 import re
-import time
 import glob
 import json
 import requests
@@ -10,16 +10,20 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 
+target_path = "/home/sakura/covid19-ishikawa/"
 
 url = 'https://www.pref.ishikawa.lg.jp/kansen/coronakennai.html'
 res = requests.get(url)
 res.encoding = res.apparent_encoding
 soup = BeautifulSoup(res.text, 'html.parser')
 
+
 tmp_contents = soup.find(id='tmp_contents')
+h1_contents = tmp_contents.find_all('h1')
+all_contents_list = h1_contents[0].find_next_siblings()
 
 def data_shaping(date):
-    m = re.match(r'[0-9]+月[0-9]+日', date_data)
+    m = re.match(r'[0-9]+月[0-9]+日', date)
     m_text = m.group()
     pos = re.split('[月日]', m_text)
     datetime_data = datetime.datetime(2020, int(pos[0]), int(pos[1]))
@@ -27,42 +31,32 @@ def data_shaping(date):
     return date
 
 df = pd.DataFrame([], columns=['date', '居住地', '年代', '性別'])
-
 date = []
 residence = []
 age = []
 sex = []
 
-
-
-h3_contents = tmp_contents.find_all('h3')
-
-for i in range(len(h3_contents)):
-    data_all = h3_contents[i].find_next_siblings()
-    if i != len(h3_contents) - 1:
-        data = data_all[:data_all.index(h3_contents[i+1])]
-        age.append(data[0].text[-3:-1])
-        sex.append(data[1].text[-2:])
-        residence.append(data[2].text[-3:].strip())
-        for n, j in enumerate(data):
-            if '陽性と判明' in str(j):
-                s = re.search(r'[0-9]+月[0-9]+日', str(j))
-                date_data = s.group(0)
-        new_date_data = data_shaping(date_data)
+table = str.maketrans('（１）', '(1)')
+all_contents = ""
+now_date = ""
+for i in all_contents_list:
+    text = i.get_text().translate(table)
+    text2 = text.replace("\xa0", "").replace("\r\n", "").replace(" ", "").replace("\u3000", "").replace("\n", "")
+    if re.search(r'令和[0-9]年+[0-9]+月[0-9]+日', text2):
+        s = re.search(r'[0-9]+月[0-9]+日', text2)
+        now_date = s.group(0)
+    if "(1)年代" in text2:
+        text_age = text2[5:-1]
+        new_date_data = data_shaping(now_date)
         date.append(new_date_data)
-    else:
-        data = data_all
-        age.append(data[0].text[-3:-1])
-        sex.append(data[1].text[-2:])
-        residence.append(data[2].text[-3:].strip())
-        for n, j in enumerate(data):
-            if '陽性と判明' in str(j):
-                s = re.search(r'[0-9]+月[0-9]+日', str(j))
-                date_data = s.group(0)
-        new_date_data = data_shaping(date_data)
-        date.append(new_date_data)
-        
-            
+        age.append(text_age)
+    elif "(2)性別" in text2:
+        text_sex = text2[5:]
+        sex.append(text_sex)
+    elif "(3)居住地" in text2:
+        text_residence = text2[6:]
+        residence.append(text_residence)
+
 c = collections.Counter(date)
 df['date'] = date
 df['居住地'] = residence
@@ -70,13 +64,15 @@ df['年代'] = age
 df['性別'] = sex
 
 patients_df = df.sort_values('date').reset_index(drop=True)
-df.to_csv('/home/sakura/covid19-ishikawa/tool/downloads/patients_data/patients.csv', index=False)
+patients_df.to_csv(target_path + 'tool/downloads/patients_data/patients.csv', index=False)
 
 # 日付データの作成
 today = datetime.datetime.now()
 this_year = today.year
 this_month = today.month
 this_day = today.day
+this_hour = today.hour
+this_minute = today.minute
 today_info = datetime.datetime(this_year, this_month, 1)
 start_date = today_info.strftime("%Y-%m-%d")
 month_count = calendar.monthrange(this_year, this_month)[1]
@@ -94,19 +90,17 @@ for num, i in enumerate(df.iloc[0:, 0]):
             df.iloc[num, 1] = c[j]
             
 # csv化
-df.to_csv('/home/sakura/covid19-ishikawa/tool/downloads/each_data/{}_{}.csv'.format(this_year, this_month), index=False)
-
-time.sleep(5)
+df.to_csv(target_path + 'tool/downloads/each_data/{}_{}.csv'.format(this_year, this_month), index=False)
 
 # 各csvを連結
-csv_files = glob.glob('/home/sakura/covid19-ishikawa/tool/downloads/each_data/*.csv')
+csv_files = glob.glob('./tool/downloads/each_data/*.csv')
 each_csv = []
 for i in csv_files:
     each_csv.append(pd.read_csv(i))
 df = pd.concat(each_csv).reset_index(drop=True).sort_values('日付')
 
 patients_summary_df = df.reset_index(drop=True)
-patients_summary_df.to_csv("/home/sakura/covid19-ishikawa/tool/downloads/final_data/total.csv", index=False)
+patients_summary_df.to_csv(target_path + 'tool/downloads/final_data/total.csv', index=False)
 
 # patientsデータの作成
 patients_df_dict = patients_df.to_dict('index')
@@ -116,7 +110,7 @@ data1 = [ patients_df_dict.get(i) for i in range(len(patients_df_dict)) ]
 patients_summary_df_dict = patients_summary_df.to_dict('index')
 data2 = [ patients_summary_df_dict.get(i) for i in range(len(patients_summary_df)) ]
 
-update_at = "{}/{}/{} {}:{}".format(this_year, this_month, this_day, today.hour, today.minute)
+update_at = "{}/{}/{} {}:{}".format(this_year, this_month, this_day, this_hour, this_minute)
 data_json = {
     "lastUpdate": update_at,
     "patients": {
@@ -129,5 +123,5 @@ data_json = {
     }
 }
 
-with open('/home/sakura/covid19-ishikawa/data/data.json', 'w') as f:
+with open(target_path + 'data/data.json', 'w') as f:
     json.dump(data_json, f, indent=4, ensure_ascii=False)
