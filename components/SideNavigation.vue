2acacae17f2961a2128b2db7ce345e90c1499451
Terachan0117 <58585239+Terachan0117@users.noreply.github.com>
2020-03-31 20:14:12 +0900

    非公式→有志による運営に文言変更

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -17,7 +17,7 @@
           />
           <div class="SideNavigation-HeaderText">
             {{ $t('menu/新型コロナウイルス感染症') }}<br />{{
-              $t('menu/対策サイト(非公式)')
+              $t('menu/対策サイト(有志による運営)')
             }}
           </div>
         </nuxt-link>
