d40e97aef50dccca4ecf19ed43bd12aa80a52750
Terachan0117 <58585239+Terachan0117@users.noreply.github.com>
2020-04-06 19:35:16 +0900

    更新

--- run-regularly.sh
+++ run-regularly.sh
@@ -1,7 +1,10 @@
-today=`date "+%Y/%m/%d %H:%M"`
+today=`date "+%Y%m%d"`
 cd /root/covid19-toyama/
 git pull origin development
-python3 /root/covid19-toyama/tool/test.py
-git add .
-git commit -m "自動更新テスト${today}"
+cd tool
+curl -O "http://opendata.pref.toyama.jp/files/covid19/${today}/toyama_patients.csv"
+curl -O "http://opendata.pref.toyama.jp/files/covid19/${today}/toyama_counts.csv"
+python test.py
+git add *
+git commit -m "Data Update ${today}"
 git push origin development
