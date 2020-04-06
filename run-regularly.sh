d113c5eccdb60e3284e692665b0860704c369e33
Terachan0117 <58585239+Terachan0117@users.noreply.github.com>
2020-04-06 14:19:04 +0900

    自動更新テスト

--- run-regularly.sh
+++ run-regularly.sh
@@ -1,7 +1,7 @@
 today=`date "+%Y/%m/%d %H:%M"`
-cd /home/sakura/covid19-ishikawa/
+cd /root/covid19-toyama/
 git pull origin development
-python3 /home/sakura/covid19-ishikawa/tool/scraping-cent.py
+python3 /root/covid19-toyama/tool/test.py
 git add .
-git commit -m "データ更新${today}"
+git commit -m "自動更新テスト${today}"
 git push origin development
