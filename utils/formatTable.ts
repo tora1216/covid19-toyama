9ff04e85f33fc8a6e6c04728644b23fec83cd038
Terachan0117 <58585239+Terachan0117@users.noreply.github.com>
2020-04-06 13:46:26 +0900

    属性に職業を追加

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -4,7 +4,8 @@ const headers = [
   { text: '公表日', value: '公表日' },
   { text: '居住地', value: '居住地' },
   { text: '年代', value: '年代' },
-  { text: '性別', value: '性別' }
+  { text: '性別', value: '性別' },
+  { text: '職業', value: '職業' }
   // { text: '退院※', value: '退院', align: 'center' }
 ]
 
@@ -13,6 +14,7 @@ type DataType = {
   居住地: string | null
   年代: string | null
   性別: '男性' | '女性' | string
+  職業: string | null
   退院: '◯' | null
   [key: string]: any
 }
@@ -22,6 +24,7 @@ type TableDataType = {
   居住地: DataType['居住地']
   年代: DataType['年代']
   性別: DataType['性別'] | '不明'
+  職業: DataType['職業'] | '職業'
   // 退院: DataType['退院']
 }
 
@@ -45,7 +48,8 @@ export default (data: DataType[]) => {
       公表日: dayjs(d.date).format('MM/DD') ?? '不明',
       居住地: d['居住地'] ?? '不明',
       年代: d['年代'] ?? '不明',
-      性別: d['性別'] ?? '不明'
+      性別: d['性別'] ?? '不明',
+      職業: d['職業'] ?? '不明'
       // 退院: d['退院']
     }
     tableDate.datasets.push(TableRow)
