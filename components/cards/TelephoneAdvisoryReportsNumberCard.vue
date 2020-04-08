2d9840d48676ea512043e733e99331b232d4910d
Terachan0117 <58585239+Terachan0117@users.noreply.github.com>
2020-04-08 11:16:02 +0900

    Url変更

--- components/cards/TelephoneAdvisoryReportsNumberCard.vue
+++ components/cards/TelephoneAdvisoryReportsNumberCard.vue
@@ -7,7 +7,7 @@
       :chart-data="contactsGraph"
       :date="Data.contacts.date"
       :unit="$t('件.reports')"
-      :url="'http://opendata.pref.toyama.jp/dataset/covid19'"
+      :url="'http://opendata.pref.toyama.jp/dataset/covid19/resource/85d4c612-c174-4f73-be63-f8512d25361a'"
     >
       <template v-slot:description>
         <ul>
