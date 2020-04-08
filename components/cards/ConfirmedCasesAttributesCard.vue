2b17894684ebdb71dd8f962441804a89eaa116ee
Terachan0117 <58585239+Terachan0117@users.noreply.github.com>
2020-04-08 10:21:04 +0900

    Fix

--- components/cards/ConfirmedCasesAttributesCard.vue
+++ components/cards/ConfirmedCasesAttributesCard.vue
@@ -46,15 +46,10 @@ export default {
     // 陽性患者の属性 中身の翻訳
     for (const row of patientsTable.datasets) {
       row['居住地'] = this.$t(row['居住地'])
+      row['年代'] = this.$t(row['年代'])
       row['性別'] = this.$t(row['性別'])
-      row['退院'] = this.$t(row['退院'])
-
-      if (row['年代'] === '10歳未満') {
-        row['年代'] = this.$t('10歳未満')
-      } else {
-        const age = row['年代'].substring(0, 2)
-        row['年代'] = this.$t('{age}代', { age })
-      }
+      // row['退院'] = this.$t(row['退院'])
+      row['職業'] = this.$t(row['職業'])
     }
 
     const data = {
