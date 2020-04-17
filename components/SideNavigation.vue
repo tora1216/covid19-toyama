b7f2a35329dc4d886573226a40a54ecdab1da9a5
Issei Terada <58585239+Terachan0117@users.noreply.github.com>
2020-04-17 13:39:32 +0900

    Change url hash

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -163,22 +163,22 @@ export default Vue.extend({
         {
           icon: 'ParentIcon',
           title: this.$t('お子様をお持ちの皆様へ'),
-          link: 'http://www.pref.toyama.jp/sections/1118/virus/index.html#3'
+          link: 'http://www.pref.toyama.jp/sections/1118/virus/index.html#5'
         },
         {
           icon: 'mdi-account-multiple',
           title: this.$t('県民の皆様へ'),
-          link: 'http://www.pref.toyama.jp/sections/1118/virus/index.html#4'
+          link: 'http://www.pref.toyama.jp/sections/1118/virus/index.html#6'
         },
         {
           icon: 'mdi-domain',
           title: this.$t('企業の皆様・はたらく皆様へ'),
-          link: 'http://www.pref.toyama.jp/sections/1118/virus/index.html#6',
+          link: 'http://www.pref.toyama.jp/sections/1118/virus/index.html#8',
           divider: true
         },
         {
           title: this.$t('富山県主催等 中止又は延期するイベント等'),
-          link: 'http://www.pref.toyama.jp/sections/1118/virus/index.html#5'
+          link: 'http://www.pref.toyama.jp/sections/1118/virus/index.html#7'
         },
         {
           title: this.$t('知事からのメッセージ'),
