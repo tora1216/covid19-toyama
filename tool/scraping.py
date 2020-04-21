c59c6fae82dfc0b5b2bab7792220168edfd1bdef
Issei Terada <s1f101803736@iniad.org>
2020-04-22 08:05:05 +0900

    Update

--- tool/scraping.py
+++ tool/scraping.py
@@ -1,9 +1,10 @@
 # coding:utf-8
 
 import json
+import mojimoji
 import codecs
 import datetime
-
+import re
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -15,7 +16,7 @@ data = json.load(codecs.open('../data/data.json', 'r', 'utf-8'))
 dt_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 data["lastUpdate"] = dt_now
 
-# 富山県HPから最新の患者一覧エクセルを取得
+# 富山県HPから最新の情報を取得
 url = "http://www.pref.toyama.jp/cms_sec/1205/kj00021798.html"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
@@ -23,11 +24,18 @@ headers = {
 r = requests.get(url, headers=headers)
 r.raise_for_status()
 soup = BeautifulSoup(r.content, "html.parser")
+
+# 要約テキストを取得
+main_list = soup.find("div", id="main")
+text = main_list.find("p").text
+summary_text = re.search(r"(.+)例（入院中(.+)例、退院(.+)例、死亡(.+)例）", text)
+
+# 一覧エクセルを取得
 file_list = soup.find("div", id="file")
 link = file_list.find("a", text="富山県内における新型コロナウイルス感染症の発生状況一覧").get("href")
 df_kanjya = pd.read_excel(link, skiprows=2)
 
-# 定義書準拠形式に変換
+# エクセル内データを定義書準拠形式に変換
 df_kanjya.rename(columns={"県番号": "No"}, inplace=True)
 df_kanjya.rename(columns={"検査結果判明日": "判明日"}, inplace=True)
 df_kanjya["判明日"] = df_kanjya["判明日"].apply(
@@ -35,6 +43,32 @@ df_kanjya["判明日"] = df_kanjya["判明日"].apply(
 )
 df_kanjya['性別'] = df_kanjya["性別"].replace("男", "男性").replace("女", "女性")
 df_kanjya['年代'] = df_kanjya["年代"].replace("90代", "90歳以上")
+
+#検査陽性者の状況
+data["main_summary"] = {
+    "date": dt_now,
+    "children": [{
+        "attr": "陽性患者数",
+        "value": int(mojimoji.zen_to_han(summary_text.group(1))),
+        "children": [{
+            "attr": "入院",
+            "value": int(mojimoji.zen_to_han(summary_text.group(2))),
+            "children": [{
+                "attr": "無症状・軽症・中等症",
+                "value": data["main_summary"]["children"][0]["children"][0]["children"][0]["value"]
+            }, {
+                "attr": "重症",
+                "value": data["main_summary"]["children"][0]["children"][0]["children"][1]["value"]
+            }]
+        }, {
+            "attr": "退院",
+            "value": int(mojimoji.zen_to_han(summary_text.group(3)))
+        }, {
+            "attr": "死亡",
+            "value": int(mojimoji.zen_to_han(summary_text.group(4)))
+        }]
+    }]
+}
 
 # 陽性患者の属性
 df_patients = df_kanjya.loc[:, ["No", "判明日", "居住地", "年代", "性別"]].fillna("-")
