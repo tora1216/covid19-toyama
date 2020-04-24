fd498eab738fe73c0e719b07df894c6d8f54546d
Issei Terada <s1f101803736@iniad.org>
2020-04-24 12:21:41 +0900

    Edit

--- components/flow/FlowPcAdvisory.vue
+++ components/flow/FlowPcAdvisory.vue
@@ -3,39 +3,44 @@
     <div :class="$style.AdvisoryContainer">
       <div :class="$style.AdvisoryContents">
         <div>
-          <span :class="$style.AdvisoryContentsTitleSmall">{{
+          <span :class="$style.AdvisoryContentsTitle">{{
             $t('帰国者・接触者相談センター')
           }}</span>
+        </div>
+        <div>
+          <div :class="[$style.AdvisoryBoxContainer, $style.AdvisoryWhiteBox]">
+            <span :class="$style.AdvisoryWhiteBoxSentense">
+              {{ $t('24時間対応') }}
+            </span>
+          </div>
         </div>
       </div>
 
       <div :class="$style.AdvisoryContents">
-      <div :class="$style.SmallerText">
-        {{ $t('8:30～17:15(※夜間・休日は、メッセージにより緊急電話番号をご案内します。)') }}
-      </div>
-        <dl>
-          <dt>{{ $t('富山市保健所(富山市)') }}</dt>
-          <dd>076-428-1152</dd>
-          <dt>{{ $t('高岡厚生センター(高岡市)') }}</dt>
-          <dd>0766-26-8414</dd>
-          <dt>{{ $t('高岡厚生センター射水支所(射水市)') }}</dt>
-          <dd>0766-56-2666</dd>
-          <dt>{{ $t('高岡厚生センター氷見支所(氷見市)') }}</dt>
-          <dd>0766-74-1780</dd>
-          <dt>{{ $t('砺波厚生センター(砺波市、南砺市)') }}</dt>
-          <dd>0763-22-3512</dd>
-          <dt>{{ $t('砺波厚生センター小矢部支所(小矢部市)') }}</dt>
-          <dd>0766-67-1070</dd>
-          <dt>{{ $t('新川厚生センター(黒部市、入善町、朝日町)') }}</dt>
-          <dd>0765-52-2647</dd>
-          <dt>{{ $t('新川厚生センター魚津支所(魚津市)') }}</dt>
-          <dd>0765-24-0359</dd>
-          <dt>{{ $t('中部厚生センター(滑川市、舟橋村、上市町、立山町)') }}</dt>
-          <dd>076-472-0637</dd>
-        </dl>
-      <div :class="$style.SmallerText">
-        {{ $t('※夜間・休日は、メッセージにより緊急電話番号をご案内します。') }}
-      </div>
+        <div class="py-8">
+          <div :class="$style.AdvisoryContentsTitle2">
+            {{ $t('所管市町村ごとに連絡先が異なります。') }}
+          </div>
+          <div
+            :class="[
+              $style.AdvisoryLink,
+              $style.AdvisoryBlockCentering,
+              'mt-4'
+            ]"
+          >
+            <a
+              href="http://www.pref.toyama.jp/cms_sec/1205/kj00021473.html"
+              target="_blank"
+              rel="noopener"
+            >
+              <span>{{ $t('各センターの電話番号はこちら') }}</span>
+              <v-icon size="18">
+                mdi-open-in-new
+              </v-icon>
+            </a>
+            <span>{{ $t('※夜間・休日は、メッセージにより緊急電話番号をご案内します。') }}</span>
+          </div>
+        </div>
       </div>
 
       <div :class="$style.AdvisoryContents">
@@ -64,15 +69,12 @@
   }
   &Contents {
     font-weight: bold;
-    // &:not(:first-child) {
-    //   border-top: 0.5px solid $gray-4;
-    // }
+    &:not(:first-child) {
+      border-top: 0.5px solid $gray-4;
+    }
     &Title {
       font-size: 26px;
       line-height: 28px;
-      &Small {
-        font-size: 20px;
-      }
     }
     &Title2 {
       font-size: 18px;
@@ -92,7 +94,7 @@
     line-height: 22px;
     text-align: left;
     a {
-      // color: rgba(0, 0, 0, 0.87);
+      color: rgba(0, 0, 0, 0.87);
       text-decoration: none;
       &:hover {
         text-decoration: underline;
@@ -114,15 +116,7 @@
     &:focus {
       color: inherit;
       text-decoration: none;
-    }
-    &:visited,
-    &:hover,
-    &:active,
-    &:focus {
-      @media screen {
-        // printだとoutlineが太くなってしまい読みにくいので、消す処理
-        outline: 1px dotted $gray-3;
-      }
+      outline: 1px dotted $gray-3;
     }
     &Icon {
       display: inline-block;
@@ -139,22 +133,13 @@
   }
   &WhiteBox {
     background-color: $white;
-    max-width: 160px;
-    padding: 8px;
+    width: 160px;
+    padding: 10px;
     &Sentense {
       color: $green-1;
-      font-size: 14px;
+      font-size: 18px;
       font-weight: bold;
     }
   }
-  dl {
-    dt {
-      font-size: 15px;
-      margin-top: 3px;
-    }
-  }
-}
-.SmallerText {
-  font-size: smaller;
 }
 </style>
