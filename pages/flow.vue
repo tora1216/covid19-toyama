fc13f19051e13cc2f8a416d608f26afccdf55524
Issei Terada <s1f101803736@iniad.org>
2020-04-24 10:51:15 +0900

    Edit

--- pages/flow.vue
+++ pages/flow.vue
@@ -5,7 +5,6 @@
       <page-header class="Flow-Heading-Title">
         {{ $t('新型コロナウイルス感染症が心配なときに') }}
       </page-header>
-      <PrinterButton :wrapper-class="'Flow-PullRight'" to="/print/flow" />
     </div>
     <div>
       <div class="only-pc">
@@ -16,12 +15,12 @@
       </div>
       <div class="Flow-Card-Button-Wrapper mt-6">
         <a
-          href="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html"
+          href="http://www.pref.toyama.jp/cms_sec/1205/kj00021473.html"
           target="_blank"
           rel="noopener"
           class="Flow-Card-Button"
         >
-          {{ $t('詳細を見る（東京都福祉保健局）') }}
+          {{ $t('詳細を見る（帰国者・接触者相談センター）') }}
           <v-icon class="Flow-Card-Button-ExternalLinkIcon" size="20">
             mdi-open-in-new
           </v-icon>
@@ -35,16 +34,13 @@
 import Vue from 'vue'
 import { TranslateResult } from 'vue-i18n'
 import CovidIcon from '@/static/covid.svg'
-import PrinterButton from '@/components/PrinterButton.vue'
 import FlowPc from '@/components/flow/FlowPc.vue'
 import FlowSp from '@/components/flow/FlowSp.vue'
 import PageHeader from '@/components/PageHeader.vue'
-
 export default Vue.extend({
   components: {
     CovidIcon,
     PageHeader,
-    PrinterButton,
     FlowPc,
     FlowSp
   },
@@ -62,69 +58,55 @@ export default Vue.extend({
 <style lang="scss">
 .Flow {
   margin-bottom: 20px;
-
   &-Heading {
     display: flex;
     align-items: center;
     margin-bottom: 12px;
-
     > svg {
       width: 30px;
       height: 30px;
-
       > path:not(:first-of-type) {
         fill: $gray-2;
       }
     }
-
     &-Title {
       margin-left: 8px;
     }
   }
-
   &-Card-Button {
     @include button-text('md');
     @include font-size(20);
-
     font-weight: bold;
     display: inline-block;
     text-decoration: none;
     color: $green-1 !important;
-
     &-Wrapper {
       text-align: center;
     }
-
     &:hover {
       color: $white !important;
     }
-
     &-ExternalLinkIcon {
       margin-left: 2px;
       color: $green-1 !important;
     }
   }
-
   &-PullRight {
     margin: 0 0 0 auto;
   }
 }
-
 @include largerThan($medium) {
   .only-sp {
     display: none;
   }
-
   .only-pc {
     display: block;
   }
 }
-
 @include lessThan($medium) {
   .only-sp {
     display: block;
   }
-
   .only-pc {
     display: none;
   }
