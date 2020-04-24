d96c9881cee72f8c780b2113734c1da67a33965b
Issei Terada <s1f101803736@iniad.org>
2020-04-24 11:34:24 +0900

    Fix

--- components/flow/FlowPcAdvisory.vue
+++ components/flow/FlowPcAdvisory.vue
@@ -3,52 +3,13 @@
     <div :class="$style.AdvisoryContainer">
       <div :class="$style.AdvisoryContents">
         <div>
-          <span :class="$style.AdvisoryContentsTitle">{{
+          <span :class="$style.AdvisoryContentsTitleSmall">{{
             $t('帰国者・接触者相談センター')
           }}</span>
         </div>
-        <div :class="[$style.AdvisoryContentsColsSentense, 'mt-4']">
-          {{ $t('帰国者・接触者 電話相談センター') }}
-        </div>
-        <div>
-          <div :class="[$style.AdvisoryBoxContainer, $style.AdvisoryWhiteBox]">
-            <span :class="$style.AdvisoryWhiteBoxSentense">
-              {{ $t('24時間対応') }}
-            </span>
-          </div>
-        </div>
       </div>
 
       <div :class="$style.AdvisoryContents">
-        <div class="py-8">
-          <div :class="$style.AdvisoryContentsTitle2">
-            {{ $t('平日(8:30～17:15)') }}
-          </div>
-          <div
-            :class="[
-              $style.AdvisoryLink,
-              $style.AdvisoryBlockCentering,
-              'mt-4'
-            ]"
-          >
-            <a
-              href="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html"
-              target="_blank"
-              rel="noopener"
-            >
-              <span>{{ $t('各保健所の電話番号は福祉保健局HPへ') }}</span>
-              <v-icon size="18">
-                mdi-open-in-new
-              </v-icon>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div :class="$style.AdvisoryContents">
-        <div :class="$style.AdvisoryContentsTitle2">
-        {{ $t('平日(8:30～17:15)') }}
-        </div>
         <dl>
           <dt>{{ $t('富山市保健所(富山市)') }}</dt>
           <dd>076-428-1152</dd>
@@ -69,7 +30,10 @@
           <dt>{{ $t('中部厚生センター(滑川市、舟橋村、上市町、立山町)') }}</dt>
           <dd>076-472-0637</dd>
         </dl>
-        {{ $t('※夜間・休日は、メッセージにより緊急電話番号をご案内します。') }}
+        {{ $t('夜間・休日は、メッセージにより緊急電話番号をご案内します。') }}
+      </div>
+
+      <div :class="$style.AdvisoryContents">
         <div v-if="!['ja', 'ja-basic'].includes($i18n.locale)" class="pt-8">
           <span>{{ $t('ひまわり') }}</span>
         </div>
@@ -85,7 +49,6 @@
   justify-content: space-between;
   height: 100%;
   color: $gray-2;
-
   &Container {
     background-color: $gray-5;
     border-radius: 4px;
@@ -94,61 +57,51 @@
     margin-bottom: 10px;
     text-align: center;
   }
-
   &Contents {
     font-weight: bold;
-
-    &:not(:first-child) {
-      border-top: 0.5px solid $gray-4;
-    }
-
+    // &:not(:first-child) {
+    //   border-top: 0.5px solid $gray-4;
+    // }
     &Title {
       font-size: 26px;
       line-height: 28px;
+      &Small {
+        font-size: 20px;
+      }
     }
-
     &Title2 {
       font-size: 18px;
     }
-
     &SubTitle {
       font-size: 18px;
     }
-
     &ColsSentense {
       line-height: 18px;
     }
   }
-
   &BlockCentering {
     display: flex;
     justify-content: center;
   }
-
   &Link {
     line-height: 22px;
     text-align: left;
-
     a {
-      color: rgba(0, 0, 0, 0.87);
+      // color: rgba(0, 0, 0, 0.87);
       text-decoration: none;
-
       &:hover {
         text-decoration: underline;
       }
     }
   }
-
   &TelephoneArea {
     display: inline-flex;
   }
-
   &Telephone {
     display: flex;
     align-items: center;
     font-size: 27px;
     font-weight: bold;
-
     &:link,
     &:visited,
     &:hover,
@@ -156,9 +109,16 @@
     &:focus {
       color: inherit;
       text-decoration: none;
-      outline: 1px dotted $gray-3;
     }
-
+    &:visited,
+    &:hover,
+    &:active,
+    &:focus {
+      @media screen {
+        // printだとoutlineが太くなってしまい読みにくいので、消す処理
+        outline: 1px dotted $gray-3;
+      }
+    }
     &Icon {
       display: inline-block;
       margin-right: 5px;
@@ -166,23 +126,25 @@
       height: 26px;
     }
   }
-
   &BoxContainer {
     border-radius: 4px;
     text-align: center;
     padding: 20px 10px;
     margin: 24px auto;
   }
-
   &WhiteBox {
     background-color: $white;
-    width: 160px;
-    padding: 10px;
-
+    max-width: 160px;
+    padding: 8px;
     &Sentense {
       color: $green-1;
-      font-size: 18px;
+      font-size: 14px;
       font-weight: bold;
+    }
+  }
+  dl {
+    dt {
+      margin-top: 20px;
     }
   }
 }
