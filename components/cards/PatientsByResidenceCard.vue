7766c3ca7b5e714c3d0a888d6d28f4576819fa4a
Issei Terada <s1f101803736@iniad.org>
2020-04-25 10:04:57 +0900

    Fix

--- components/cards/PatientsByResidenceCard.vue
+++ components/cards/PatientsByResidenceCard.vue
@@ -1,11 +1,11 @@
-<template>
+  <template>
   <v-col cols="12" md="6" class="DataCard">
     <agency-bar-chart
       :title="$t('陽性患者数(居住地別)')"
       :title-id="'number-of-confirmed-cases-by-residence'"
       :chart-id="'time-bar-chart-patients-by-residence'"
-      :chart-data="residenceData"
-      :date="residenceData.date"
+      :chart-data="agencyData"
+      :date="agencyData.date"
       :url="'http://www.pref.toyama.jp/cms_sec/1205/kj00021798.html'"
       :unit="$t('人')"
     >
@@ -15,27 +15,24 @@
 
 <script>
 import Data from '@/data/data.json'
-import AgencyBarChart from '@/components/AgencyBarChart.vue'
-
+import ResidenceBarChart from '@/components/ResidenceBarChart.vue'
+const agencyData=Data.patients_by_residence
 export default {
   components: {
     AgencyBarChart
   },
   data() {
-    // 居住地別陽性患者数グラフ
-    const residenceData = Data.patients_by_residence
-
-    const labels = [
+    const agencies = [
       this.$t('男性'),
       this.$t('女性'),
       this.$t('その他')
     ]
-    ageData.datasets.map(dataset => {
+    agencyData.datasets.map(dataset => {
       dataset.label = this.$t(dataset.label)
     })
     return {
-      residenceData,
-      labels
+      agencyData,
+      agencies
     }
   }
 }
