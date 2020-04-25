4d7453961ce5ed31502d40d57625cd5a81c84b88
Issei Terada <s1f101803736@iniad.org>
2020-04-25 09:28:55 +0900

    Change bar chart -> stacked bar chart

--- layouts/default.vue
+++ layouts/default.vue
@@ -87,7 +87,7 @@ export default Vue.extend({
       link: [
         {
           rel: 'canonical',
-          href: `https://stopcovid19-toyama.netlify.com${this.$route.path}`
+          href: `https://stopcovid19-toyama.netlify.app${this.$route.path}`
         }
       ],
       meta: [
@@ -116,7 +116,7 @@ export default Vue.extend({
         {
           hid: 'og:url',
           property: 'og:url',
-          content: `https://stopcovid19-toyama.netlify.com${this.$route.path}`
+          content: `https://stopcovid19-toyama.netlify.app${this.$route.path}`
         },
         {
           hid: 'og:locale',
