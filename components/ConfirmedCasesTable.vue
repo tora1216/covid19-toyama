3c510c12ceec2c896972464bf8128cb5653fd72e
Issei Terada <s1f101803736@iniad.org>
2020-04-28 18:25:19 +0900

    Fix

--- components/ConfirmedCasesTable.vue
+++ components/ConfirmedCasesTable.vue
@@ -14,26 +14,50 @@
         </div>
       </div>
       <ul :class="$style.group">
-        <li :class="[$style.box, $style.deceased]">
+        <li :class="[$style.box, /*$style.parent,*/ $style.hospitalized]">
           <div :class="$style.pillar">
             <div :class="$style.content">
               <span>
-              {{ $t('入院中') }}・
-               <br />{{ $t('入院等調整中') }}
-              </span>
+                {{ $t('入院中') }}・
+                <br />{{ $t('入院等調整中') }}
+             </span>
               <span>
                 <strong>{{ 入院中 }}</strong>
                 <span :class="$style.unit">{{ $t('人') }}</span>
               </span>
             </div>
           </div>
+          <!-- <ul :class="$style.group">
+            <li :class="[$style.box, $style.short, $style.minor]">
+              <div :class="$style.pillar">
+                <div :class="$style.content">
+                  eslint-disable vue/no-v-html
+                  <span v-html="$t('軽症・<br />中等症')" />
+                  eslint-enable vue/no-v-html
+                  <span>
+                    <strong>{{ 軽症中等症 }}</strong>
+                    <span :class="$style.unit">{{ $t('人') }}</span>
+                  </span>
+                </div>
+              </div>
+            </li>
+            <li :class="[$style.box, $style.short, $style.severe]">
+              <div :class="$style.pillar">
+                <div :class="$style.content">
+                  <span>{{ $t('重症') }}</span>
+                  <span>
+                    <strong>{{ 重症 }}</strong>
+                    <span :class="$style.unit">{{ $t('人') }}</span>
+                  </span>
+                </div>
+              </div>
+            </li>
+          </ul> -->
         </li>
         <li :class="[$style.box, $style.deceased]">
           <div :class="$style.pillar">
             <div :class="$style.content">
-              <span>
-              {{ $t('死亡') }}
-              </span>
+              <span>{{ $t('死亡') }}</span>
               <span>
                 <strong>{{ 死亡 }}</strong>
                 <span :class="$style.unit">{{ $t('人') }}</span>
@@ -44,9 +68,7 @@
         <li :class="[$style.box, $style.recovered]">
           <div :class="$style.pillar">
             <div :class="$style.content">
-              <span>
-              {{ $t('退院') }}
-              </span>
+              <span>{{ $t('退院') }}</span>
               <span>
                 <strong>{{ 退院 }}</strong>
                 <span :class="$style.unit">{{ $t('人') }}</span>
@@ -72,6 +94,14 @@ export default Vue.extend({
       type: Number,
       required: true
     },
+    // 軽症中等症: {
+    //   type: Number,
+    //   required: true
+    // },
+    // 重症: {
+    //   type: Number,
+    //   required: true
+    // },
     死亡: {
       type: Number,
       required: true
@@ -171,37 +201,41 @@ $default-boxdiff: 35px;
   &.confirmed {
     width: 100%;
     > .pillar {
-      // [4列] 1/4
+      // [6列] 1/6
+      // width: calc((100% + #{$default-bdw} * 2) / 6 - #{$default-bdw} * 3);
       width: calc((100% + #{$default-bdw} * 2) / 4 - #{$default-bdw} * 3);
     }
     > .group {
-      // [4列] 3/4
+      // [6列] 5/6
+      // width: calc((100% + #{$default-bdw} * 2) / 6 * 5 + #{$default-bdw});
       width: calc((100% + #{$default-bdw} * 2) / 4 * 3 + #{$default-bdw});
     }
   }
   &.hospitalized {
     margin-left: $default-bdw;
-    // [3列] 3/3
-    width: calc(100% / 4 * 1 - #{$default-bdw});
-    > .pillar {
-      // [3列] 1/3
-      width: calc((100% + #{$default-bdw} * 2) / 3 - #{$default-bdw} * 3);
-    }
-    > .group {
-      // [3列] 1/3
-      width: calc((100% + #{$default-bdw} * 2) / 3 * 1 + #{$default-bdw});
-    }
+    // [5列] 3/5
+    // 内側のカードの幅（3枚なら３当分）
+    width: calc(100% / 3 * 1 - #{$default-bdw});
+    // > .pillar {
+    //   // [3列] 1/3
+    //   width: calc((100% + #{$default-bdw} * 2) / 3 - #{$default-bdw} * 3);
+    // }
+    // > .group {
+    //   // [3列] 2/3
+    //   width: calc((100% + #{$default-bdw} * 2) / 3 * 2 + #{$default-bdw});
+    // }
   }
-  &.minor,
-  &.severe {
-    margin-left: $default-bdw;
-    // [2列] 1/2
-    width: calc(100% / 2 - #{$default-bdw});
-  }
+  // &.minor,
+  // &.severe {
+  //   margin-left: $default-bdw;
+  //   // [2列] 1/2
+  //   width: calc(100% / 2 - #{$default-bdw});
+  // }
   &.deceased,
   &.recovered {
     margin-left: $default-bdw;
     // [5列] 1/5
+    // width: calc(100% / 5 - #{$default-bdw});
     width: calc(100% / 3 - #{$default-bdw});
   }
 }
@@ -214,7 +248,6 @@ $default-boxdiff: 35px;
   align-items: center;
   > span {
     display: block;
-    width: 100%;
     @include font-size(16);
     &:last-child {
       margin-top: 0.1em;
@@ -272,66 +305,74 @@ $default-boxdiff: 35px;
     }
     &.confirmed {
       > .pillar {
+        // width: calc(
+        //   (100% + #{px2vw($bdw, $vw)} * 2) / 6 - #{px2vw($bdw, $vw)} * 3
+        // );
         width: calc(
-          (100% + #{px2vw($bdw, $vw)} * 2) / 6 - #{px2vw($bdw, $vw)} * 3
+          (100% + #{px2vw($bdw, $vw)} * 2) / 4 - #{px2vw($bdw, $vw)} * 3
         );
       }
       > .group {
+        // width: calc(
+        //   (100% + #{px2vw($bdw, $vw)} * 2) / 6 * 5 + #{px2vw($bdw, $vw)}
+        // );
         width: calc(
-          (100% + #{px2vw($bdw, $vw)} * 2) / 6 * 5 + #{px2vw($bdw, $vw)}
+          (100% + #{px2vw($bdw, $vw)} * 2) / 4 * 3 + #{px2vw($bdw, $vw)}
         );
       }
     }
     &.hospitalized {
       margin-left: px2vw($bdw, $vw);
-      width: calc(100% / 5 * 3 - #{px2vw($bdw, $vw)});
-      > .pillar {
-        width: calc(
-          (100% + #{px2vw($bdw, $vw)} * 2) / 3 - #{px2vw($bdw, $vw)} * 3
-        );
-      }
-      > .group {
-        width: calc(
-          (100% + #{px2vw($bdw, $vw)} * 2) / 3 * 2 + #{px2vw($bdw, $vw)}
-        );
-      }
+      // width: calc(100% / 5 * 3 - #{px2vw($bdw, $vw)});
+      width: calc(100% / 3 * 1 - #{px2vw($bdw, $vw)});
+      // > .pillar {
+      //   width: calc(
+      //     (100% + #{px2vw($bdw, $vw)} * 2) / 3 - #{px2vw($bdw, $vw)} * 3
+      //   );
+      // }
+      // > .group {
+      //   width: calc(
+      //     (100% + #{px2vw($bdw, $vw)} * 2) / 3 * 2 + #{px2vw($bdw, $vw)}
+      //   );
+      // }
     }
-    &.minor,
-    &.severe {
-      margin-left: px2vw($bdw, $vw);
-      width: calc(100% / 2 - #{px2vw($bdw, $vw)});
-    }
+    // &.minor,
+    // &.severe {
+    //   margin-left: px2vw($bdw, $vw);
+    //   width: calc(100% / 2 - #{px2vw($bdw, $vw)});
+    // }
     &.deceased,
     &.recovered {
       margin-left: px2vw($bdw, $vw);
-      width: calc(100% / 5 - #{px2vw($bdw, $vw)});
+      // width: calc(100% / 5 - #{px2vw($bdw, $vw)});
+      width: calc(100% / 3 - #{px2vw($bdw, $vw)});
     }
   }
 }
 // variables.scss Breakpoints: huge (1440)
-// @include lessThan(1440) {
-//   @include override(1440, 3, 15, 150, 35);
-// }
+@include lessThan(1440) {
+  @include override(1440, 3, 15, 150, 35);
+}
 // Vuetify Breakpoints: Large (1264)
-// @include lessThan(1263) {
-//   @include override(1263, 2, 13, 107, 24);
-// }
+@include lessThan(1263) {
+  @include override(1263, 2, 13, 107, 24);
+}
 // variables.scss Breakpoints: large (1170)
-// @include lessThan(1170) {
-//   @include override(1170, 2, 13, 107, 24);
-// }
+@include lessThan(1170) {
+  @include override(1170, 2, 13, 107, 24);
+}
 // Vuetify Breakpoints: Small (960)
-// @include lessThan(959) {
-//   @include override(960, 4, 14, 180, 40);
-// }
-// @include lessThan(767) {
-//   @include override(960, 3, 14, 180, 40);
-// }
+@include lessThan(959) {
+  @include override(960, 4, 14, 180, 40);
+}
+@include lessThan(767) {
+  @include override(960, 3, 14, 180, 40);
+}
 // Vuetify Breakpoints: Extra Small (600)
-// @include lessThan(600) {
-//   @include override(600, 3, 14, 150, 35);
-// }
-// @include lessThan(420) {
-//   @include override(600, 2, 12, 150, 35);
-// }
+@include lessThan(600) {
+  @include override(600, 3, 14, 150, 35);
+}
+@include lessThan(420) {
+  @include override(600, 2, 12, 150, 35);
+}
 </style>
