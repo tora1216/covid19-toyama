f716623134492d20b49c0a13e5d6b6a748ff109c
Issei Terada <s1f101803736@iniad.org>
2020-05-16 08:16:17 +0900

    fix style

--- pages/index.vue
+++ pages/index.vue
@@ -26,14 +26,12 @@
       <h3 class="WhatsNew-heading"><i aria-hidden="true" class="v-icon notranslate WhatsNew-heading-icon mdi mdi-bullhorn theme--light" style="font-size: 24px;"></i>
         {{ $t('富山県対策指針 策定') }} 
       </h3>
-      <div style="border: 4px solid #ffd700;">
-      <p>富山県は現在「Stage２」の措置を実施しています。</p>
+      <div style="background-color: #ffe200; color: #4d4d4d; border-radius: 4px; padding: 4px 8px; font-weight: bold; margin: 16px 0px;">
+      現在「Stage２」の措置を実施しています
       </div>
-      <p>富山県では、外出自粛や休業要請を定めた３段階のステージを設定しています。</p>
-      <p>直近１週間の新規感染者数など５項目の指標を設けており、全ての指標が基準を下回る状況が続けば、政府の方針も踏まえつつ、ステージを段階的に変えていきます。</p>
+      <p>富山県では、外出自粛や休業要請を定めた３段階のステージを設定しています。直近１週間の新規感染者数など５項目の指標を設けており、全ての指標が基準を下回る状況が続けば、政府の方針も踏まえつつ、ステージを段階的に変えていきます。</p>
       <img class="EmergencyImage" src="/roadmap1.jpg" alt="roadmap1" />
       <img class="EmergencyImage" src="/roadmap2.jpg" alt="roadmap2" />
-      <a href="http://www.pref.toyama.jp/cms_sec/1205/kj00022037.html" target="_blank">{{ $t('新型コロナウイルス感染拡大にかかる富山県対策指針について（令和2年5月14日策定）') }}</a>
     </div>
     <static-info
       class="mb-4"
