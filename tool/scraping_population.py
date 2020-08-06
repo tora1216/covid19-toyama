d4459172ee42d2b74ecb8fdc5e0c15b6aa97c9da
Issei Terada <s1f101803736@iniad.org>
2020-08-07 01:45:53 +0900

    Add cron to population data

--- tool/scraping_population.py
+++ tool/scraping_population.py
@@ -10,11 +10,12 @@ data["lastUpdate"] = dt_now
 f = open('../data/data.json', 'w', encoding='utf-8')
 json.dump(data, f, ensure_ascii=False, indent=4)
 
-# モバイル空間統計より最新データ取得
+# NTTモバイル空間統計より最新データ取得
 url = 'https://mobaku.jp/covid-19/download/%E5%A2%97%E6%B8%9B%E7%8E%87%E4%B8%80%E8%A6%A7.csv'
 df = pd.read_csv(url, encoding='SHIFT-JIS')
 df = df.iloc[176:180, -7:]
 
+# 整形
 df = df.to_dict(orient='list')
 datasets = []
 for key, value in df.items(): 
@@ -22,7 +23,6 @@ for key, value in df.items():
         "label": key,
         "data": value
     })
-
 data = {
     "date": dt_now,
     "datasets": datasets,
@@ -34,5 +34,6 @@ data = {
     ]
 }
 
+# 最新情報に上書き
 f = open('../data/population.json', 'w', encoding='utf-8')
 json.dump(data, f, ensure_ascii=False, indent=4)
