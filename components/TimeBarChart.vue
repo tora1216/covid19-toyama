f5292481f5a6c05f12321c99bd8c7d841d39aa9d
Terachan <contact@tera-chan.com>
2020-08-22 17:07:23 +0900

    Fix

--- components/TimeBarChart.vue
+++ components/TimeBarChart.vue
@@ -39,7 +39,7 @@
       class="cardTable"
       item-key="name"
     />
-    <template v-slot:infoPanel>
+    <template v-if="showButton" v-slot:infoPanel>
       <data-view-basic-info-panel
         :l-text="displayInfo.lText"
         :s-text="displayInfo.sText"
