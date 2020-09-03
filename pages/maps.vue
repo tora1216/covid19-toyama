7d1a424d0423b459b247ec9a2a1d4361b3fbb97c
Terachan <contact@tera-chan.com>
2020-09-03 10:47:48 +0900

    Fix

--- pages/maps.vue
+++ pages/maps.vue
@@ -5,10 +5,10 @@
     </page-header>
   
     <StaticCard>
-     <ul>
-       <li>{{ $t('※内閣官房新型コロナウイルス感染症対策推進室のホームページで公開されている医療機関の状況を元にしています。') }}</li>
+     <ul class="note">
+       <li>{{ $t('※内閣官房新型コロナウイルス感染症対策推進室のホームページにて公開されている医療機関の状況を元にしています。') }}</li>
        <li>{{ $t('※都道府県の協力により調査対象医療機関を日々追加していますが、すべての入院病床を有する病院の情報を網羅しているものではありません。') }}</li>
-       <li>{{ $t('※この地図に載っていない医療機関がある場合には、その病院のホームページ等でご確認ください。') }}</li>
+       <li>{{ $t('※この地図に載っていない医療機関がある場合は、その病院のホームページ等でご確認ください。') }}</li>
      </ul>
      <iframe class="maps" src="https://tera-chan.com/covid19-toyama/maps/" allowfullscreen></iframe>
     </StaticCard>
@@ -36,8 +36,14 @@ export default Vue.extend({
 
 <style>
 .maps{
-  height:70vh;
+  height:60vh;
   width:100%;
   border:none;
+}
+.note{
+  list-style-type: none;
+  padding-left: 0 !important;
+  font-size: 12px;
+  color: #707070;
 }
 </style>
