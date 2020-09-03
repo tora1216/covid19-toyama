571cbab15d7a68051dba16c714ffd73313a6f9c3
Terachan <contact@tera-chan.com>
2020-09-03 10:39:47 +0900

    Add note

--- pages/maps.vue
+++ pages/maps.vue
@@ -5,6 +5,11 @@
     </page-header>
   
     <StaticCard>
+     <ul>
+       <li>{{ $t('※内閣官房新型コロナウイルス感染症対策推進室のホームページで公開されている医療機関の状況を元にしています。') }}</li>
+       <li>{{ $t('※都道府県の協力により調査対象医療機関を日々追加していますが、すべての入院病床を有する病院の情報を網羅しているものではありません。') }}</li>
+       <li>{{ $t('※この地図に載っていない医療機関がある場合には、その病院のホームページ等でご確認ください。') }}</li>
+     </ul>
      <iframe class="maps" src="https://tera-chan.com/covid19-toyama/maps/" allowfullscreen></iframe>
     </StaticCard>
   </div>
