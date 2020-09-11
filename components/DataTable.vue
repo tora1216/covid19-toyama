7d3002fcde46c7cb0b095d092ce29430db281226
Terachan <contact@tera-chan.com>
2020-09-11 12:55:54 +0900

    Fix note

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -4,7 +4,7 @@
       <span />
     </template>
     <template v-slot:description>
-      {{ $t('（注）No406・No407の事例は、患者発生届が取り下げられているため陽性患者に含まれない') }}
+      {{ $t('（注）No406・No407の事例は、患者発生届が取り下げられている') }}
     </template>
     <v-data-table
       :ref="'displayedTable'"
